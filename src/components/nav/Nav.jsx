55abb1cbc2311e536981848ea5a5fcfa43028ec7
Raphaelmary <34437033+Raphaelmary@users.noreply.github.com>
2023-04-03 00:14:42 +0100

    sass activated

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <>
+        <nav className='nav__bar'>
+            <h1>Nav</h1>
+        </nav>
+    </>
+  )
+}
+
+export default Nav;
